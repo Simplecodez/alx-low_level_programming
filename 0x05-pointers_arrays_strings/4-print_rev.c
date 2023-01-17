ea6f5e11da62a94330b69352a7fe5533108dab2c
Simplecodez <elenduemmanuelelano@gmail.com>
2023-01-16 17:41:21 -0800

    great!

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+/*
+ * File: 4-print_rev.c
+ * Auth: Timothy Victor
+ */
+
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse.
+ * @s: The string to be printed.
+ */
+void print_rev(char *s)
+{
+
+int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+		_putchar(s[i]);
+	_putchar('\n');
+
+}
